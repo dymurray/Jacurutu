ab6cf6ba0071ae0e20b7b1439b64f5dbd24f9293
Dylan Murray <dymurray@redhat.com>
2016-10-11 13:51:29 -0400

    Added more dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(
-        name="Jucarutu",
-        version="0.1",
+        name='Jucarutu',
+        version='0.1',
+        url='https://github.com/dymurray/jucarutu',
+        author='Dylan Murray',
         install_requires=[
-            "project-free-tv",
-            "pychromecast",
-            "cast",
+            'project-free-tv',
+            'pychromecast',
+            'cast',
+            'docopt',
          ],
 )
