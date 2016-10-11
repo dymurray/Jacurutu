9a91dcbd31be839075f44c8a1ca4b545f2303fa3
Connor Murray <thawklax22@gmail.com>
2016-10-11 12:08:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name="Jucarutu",
-        version="1.0",
+        version="0.1",
         install_requires=[
             "project-free-tv",
             "pychromecast",
