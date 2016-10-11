1e1d355fd65680aee284f815c1944d6a915e25a7
Dylan Murray <dymurray@redhat.com>
2016-10-11 12:57:07 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name="Jucarutu",
+        version="1.0",
+        install_requires=[
+            "project-free-tv",
+            "pychromecast",
+            "cast",
+         ],
+)
